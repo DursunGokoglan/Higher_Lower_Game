4ae98d49aa0a81601171860562441db9b9486702
Dursun <dursungokoglan@gmail.com>
2023-04-16 13:13:26 +0300

    Some code improvements done

--- higherlower.py
+++ higherlower.py
@@ -1,7 +1,3 @@
-# Make B the new A every round
-# Think about removing used ones
-# Add more elements to the gamedata if you want
-
 import random
 from gamedata import data
 
@@ -10,10 +6,13 @@ print("Welcome to HigherLower! You will guess which one has more followers on In
 found = True
 score = 0
 
+a = data[random.randint(0, len(data) - 1)]
+data.remove(a)
+
 while found:
 
-    a = data[random.randint(0, len(data) - 1)]
     b = data[random.randint(0, len(data) - 1)]
+    data.remove(b)
 
     f_a = a['follower_count']
     f_b = b['follower_count']
@@ -23,6 +22,8 @@ while found:
     else:
         answer = "B"
 
+    print(f"Psst! The answer is {answer}")
+
     print(f"Your score is {score}")
     print(f"A is {a['name']}, a {a['description']} from {a['country']}.")
     print(f"B is {b['name']}, a {b['description']} from {b['country']}.")
@@ -31,6 +32,7 @@ while found:
 
     if pick == answer:
         score += 1
+        a = b
 
     else:
         found = False
